d0f0be6258f8cf5f36ee564685bf273b9d4715e7
Yeray SG <46818206+YeraySG@users.noreply.github.com>
2019-01-23 18:01:28 +0100

    STFT
    Xl, Xr calculado de varias formas no da lo mismo. STFT de la señal y del Canal da cosas distintas

--- testing.py
+++ testing.py
@@ -5,6 +5,7 @@ Created on Tue Jan 22 18:54:07 2019
 @author: u114293
 """
 #import soundfile
+import scipy.signal as sp
 import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
@@ -16,7 +17,7 @@ plt.plot(data)
 
 Xl = data[:,0] # Left Channel
 Xr = data[:,1] # Right Channel
-
+"""
 plt.figure()
 plt.plot(Xl)
 plt.title('Left Channel')
@@ -24,15 +25,20 @@ plt.title('Left Channel')
 plt.figure()
 plt.plot(Xr)
 plt.title('Right Channel')
+"""
+STFTdata  = sp.stft(data,samplerate,'hann',256)
+STFTdataXl  = sp.stft(Xl,samplerate,'hann',256)
 
 AutoLeft = np.linalg.norm(Xl,ord=2)**2 #Squared Norm of the Vector Xl
-#AutoLeft2 = np.sum(Xl.conj().T*Xl)
+AutoLeft2 = np.sum(Xl.conj().T*Xl)
 AutoLeft3 = np.sum(np.transpose(Xl)*Xl)
+AutoLeft4 = np.dot(Xl,Xl)
 
 AutoRight = np.linalg.norm(Xr,ord=2)**2 #Squared Norm of the Vector Xr
-#AutoRight2 = np.sum(Xr.conj().T*Xr)
-
+AutoRight2 = np.sum(Xr.conj().T*Xr)
+AutoRight3 = np.sum(np.transpose(Xr)*Xr)
+AutoRight4 = np.dot(Xr,Xr)
 Cross = Xl.conj().T*Xr  #conjugada traspuesta de Xl * Xr
 CrossCoefficient = Cross/(np.linalg.norm(Xl)*np.linalg.norm(Xr)) #Cross Correlation Coefficient
-#CrossCoefficient2 = Cross/np.sqrt(AutoLeft2*AutoRight2) 
+CrossCoefficient2 = Cross/np.sqrt(AutoLeft2*AutoRight2) 
 Dif = sum (CrossCoefficient-CrossCoefficient2)
